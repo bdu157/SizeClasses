4b38be4d01d462eec859290d0b99fd6eb954f040
Dongwoo Pae <dongwoopae@gmail.com>
2019-12-06 12:31:52 +0900

    completed guided project

---
+++ SizeClasses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClasses
+//
+//  Created by Dongwoo Pae on 12/6/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
